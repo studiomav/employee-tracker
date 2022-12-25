6392cb0a17890f578117df3f215d32fa9627bb2b
Michael Virga <michael@studiomav.com>
2022-12-24 19:39:51 -0800

    finished query input and updates

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const log = require('why-is-node-running');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
@@ -74,6 +75,7 @@ function addDepartment()
         db.query(`INSERT INTO DEPARTMENTS (DEPARTMENT_NAME)
         VALUES ("${answers.department_name}");`, function (err, results)
         {
+            console.log();
             console.log(`Added ${answers.department_name} to the departments table`)
             mainMenu();
         });
@@ -82,45 +84,168 @@ function addDepartment()
 
 function addRole()
 {
-    inquirer.prompt(
-    [{
-        type: 'input',
-        name: 'role_name',
-        message: 'What is the name of the new role?',
-        validate: isAnswerBlank,
-    },
+    var deps = [];
+    var depnames = [];
+    //get departments
+    db.query('SELECT ID, DEPARTMENT_NAME FROM DEPARTMENTS;', function (err, results)
     {
-        type: 'input',
-        name: 'salary',
-        message: 'What is the salary amount for this new role?',
-        validate: isAnswerBlank,
-    },
-    {
-        type: 'input',
-        name: 'department_id',
-        message: 'What is the department ID for this new role?',
-        validate: isAnswerBlank,
-    }]).then((answers) =>
-    {
-        db.query(`INSERT INTO ROLES (ROLE_NAME, SALARY, DEPARTMENT_ID)
-        VALUES ("${answers.role_name}", ${answers.salary}, ${answers.department_id});`, function (err, results)
+        console.log();
+        results.forEach(element =>
         {
-            console.log(`Added ${answers.role_name} to the roles table with a salary of ${answers.salary} and department ID of ${answers.department_id}`)
-            mainMenu();
+            deps.push(element);
+            depnames.push(element.DEPARTMENT_NAME);
+        });
+
+        inquirer.prompt(
+        [{
+            type: 'input',
+            name: 'role_name',
+            message: 'What is the name of the new role?',
+            validate: isAnswerBlank,
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary amount for this new role?',
+            validate: isAnswerBlank,
+        },
+        {
+            type: 'list',
+            name: 'department_name',
+            message: 'What is the department for this new role?',
+            choices: depnames,
+        }]).then((answers) =>
+        {
+            let depid = deps.find(dep => dep.DEPARTMENT_NAME == answers.department_name).ID;
+            db.query(`INSERT INTO ROLES (ROLE_NAME, SALARY, DEPARTMENT_ID)
+            VALUES ("${answers.role_name}", ${answers.salary}, ${depid});`, function (err, results)
+            {
+                console.log();
+                console.log(`Added ${answers.role_name} to the roles table with a salary of ${answers.salary} and department of ${answers.department_name}.`)
+                mainMenu();
+            });
         });
     });
 }
 
 function addEmployee()
 {
-    console.log('addEmployee()');
-    mainMenu();
+    var roles = [];
+    var rolenames = [];
+    var mngrs = [];
+    var mngrnames = [];
+    //get roles
+    db.query('SELECT ID, ROLE_NAME FROM ROLES;', function (err, results)
+    {
+        results.forEach(element =>
+        {
+            roles.push(element);
+            rolenames.push(element.ROLE_NAME);
+        });
+
+        //get managers
+        db.query('SELECT ID, FIRST_NAME, LAST_NAME FROM EMPLOYEES WHERE MANAGER_ID IS NULL;', function (err, results)
+        {
+            results.forEach(element =>
+            {
+                mngrs.push(element);
+                mngrnames.push(`${element.FIRST_NAME} ${element.LAST_NAME}`);
+            });
+
+            inquirer.prompt(
+            [{
+                type: 'input',
+                name: 'first_name',
+                message: 'What is the first name of the new employee?',
+                validate: isAnswerBlank,
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: 'What is the last name of the new employee?',
+                validate: isAnswerBlank,
+            },
+            {
+                type: 'list',
+                name: 'role_name',
+                message: 'What is the role ID for this new employee?',
+                choices: rolenames,
+                validate: isAnswerBlank,
+            },
+            {
+                type: 'list',
+                name: 'manager_name',
+                message: 'Who is this new employee\`s manager? Leave blank if they are a manager.',
+                choices: mngrnames,
+            }]).then((answers) =>
+            {
+                let roleid = roles.find(role => role.ROLE_NAME == answers.role_name).ID;
+                let mngrid = mngrs.filter(mngr => [mngr.FIRST_NAME, mngr.LAST_NAME].join(' ') == answers.manager_name)[0].ID;
+        
+                db.query(`INSERT INTO EMPLOYEES (FIRST_NAME, LAST_NAME, ROLE_ID, MANAGER_ID)
+                VALUES ("${answers.first_name}", "${answers.last_name}", ${roleid}, ${mngrid || "null"});`, function (err, results)
+                {
+                    console.log();
+                    console.log(`Added ${answers.first_name} ${answers.last_name} to the employees table with the role ${answers.role_name} and manager ${answers.manager_name}.`)
+                    mainMenu();
+                });
+            });
+        });
+    });
 }
 
 function updateRole()
 {
-    console.log('updateRole()');
-    mainMenu();
+    var emps = [];
+    var empnames = [];
+    var roles = [];
+    var rolenames = [];
+
+    //get employees
+    db.query('SELECT ID, FIRST_NAME, LAST_NAME FROM EMPLOYEES;', function (err, results)
+    {
+        results.forEach(element =>
+        {
+            emps.push(element);
+            empnames.push(`${element.FIRST_NAME} ${element.LAST_NAME}`);
+        });
+
+        //get roles
+        db.query('SELECT ID, ROLE_NAME FROM ROLES;', function (err, results)
+        {
+            results.forEach(element =>
+            {
+                roles.push(element);
+                rolenames.push(element.ROLE_NAME);
+            });
+
+            inquirer.prompt(
+            [{
+                type: 'list',
+                name: 'employee_name',
+                message: 'Select an employee to update:',
+                choices: empnames,
+            },
+            {
+                type: 'list',
+                name: 'role_name',
+                message: 'Select the new role for this employee:',
+                choices: rolenames,
+            }]).then((answers) =>
+            {
+                let empid = emps.filter(emp => [emp.FIRST_NAME, emp.LAST_NAME].join(' ') == answers.employee_name)[0].ID;
+                let roleid = roles.find(role => role.ROLE_NAME == answers.role_name).ID;
+        
+                db.query(`UPDATE (EMPLOYEES)
+                SET ROLE_ID = ${roleid} WHERE ID = ${empid};`, function (err, results)
+                {
+                    console.log();
+                    console.log(`Updated ${answers.employee_name} to role ${answers.role_name}`)
+                    mainMenu();
+                });
+            });
+        });
+    });
 }
 
 function mainMenu()
@@ -140,6 +265,7 @@ function mainMenu()
                     'Add a role',
                     'Add an employee',
                     'Update an employee role',
+                    'Exit'
                 ],
             }
         ]
@@ -167,6 +293,10 @@ function mainMenu()
                 break;
             case 'Update an employee role':
                 updateRole();
+                break;
+            case 'Exit':
+                console.log('Goodbye.');
+                db.end();
                 break;
             default:
                 console.log('error with response ' + response.menuChoice);
